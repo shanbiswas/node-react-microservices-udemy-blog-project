e67b6baa48a156bed56862d63bf36f443d65b083
Santanu Biswas <shanpro.2015@gmail.com>
2023-12-26 14:16:08 +0530

    CORS error fixed

--- comments/index.js
+++ comments/index.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {
-  res.send(commentsByPostId[req.params.id] || []);
-});
+// app.get("/posts/:id/comments", (req, res) => {
+//   res.send(commentsByPostId[req.params.id] || []);
+// });
 
 app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
--- posts/index.js
+++ posts/index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const posts = {};
 
